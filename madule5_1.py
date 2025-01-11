b9b7c1a59a6379561ae29229dcafa7cb7cda0c4a
yarik333777 <132485222+yarik333777@users.noreply.github.com>
2025-01-11 16:21:03 +0900

    Update madule5_1.py

--- madule5_1.py
+++ madule5_1.py
@@ -6,18 +6,19 @@ class House:
 
     def go_to(self, new_floors):
         self.new_floors = new_floors
-        if self.number_of_floors >= -1:
+        if self.number_of_floors >= 1:
+
             if self.new_floors > self.number_of_floors:
                 print('this isn,t floors')
             else:
-                print(self.new_floors)
+                for i in range(1, self.new_floors):
+                    print(i)
 
 
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
+h1.go_to(6)
 # Объект этого класса должен обладать следующими атрибутами:
 # self.name - имя, self.number_of_floors - кол-во этажей
 # Также должен обладать методом go_to(new_floor), где new_floor - номер этажа(int), на который нужно приехать.
